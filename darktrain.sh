cf3b986348d6fe31fdb052670b7128670c439fc9
guoruili <guoruili@3in.org>
2021-01-31 19:11:55 +0800

    change bash

--- darktrain.sh
+++ darktrain.sh
@@ -4,7 +4,7 @@ JPG_FOLDER=${1%*/}
 XML_FOLDER=${2%*/}
 DES_FOLDER=$JPG_FOLDER"_after"
 
-python3 archive.py -j "$JPG_FOLDER" -x "$XML_FOLDER" -d "$DES_FOLDER"
-python3 cleaner.py -j "$DES_FOLDER/JPEGImages" -x "$DES_FOLDER/Annotations"
-python3 classify.py -x "$DES_FOLDER/Annotations" -d "$DES_FOLDER"
-python3 prepare.py -d "$DES_FOLDER"
+python archive.py -j "$JPG_FOLDER" -x "$XML_FOLDER" -d "$DES_FOLDER"
+python cleaner.py -j "$DES_FOLDER/JPEGImages" -x "$DES_FOLDER/Annotations"
+python classify.py -x "$DES_FOLDER/Annotations" -d "$DES_FOLDER"
+python prepare.py -d "$DES_FOLDER"
